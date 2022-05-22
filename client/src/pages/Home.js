843ac4e425f59be1e99c0781b368d8763b8e44ab
CD MacBook <corydavis1989@gmail.com>
2022-05-22 16:39:38 -0500

    thought rendering on page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import React from "react";
+import ThoughtList from "../components/ThoughtList";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
   //use useQuery hook to make query request
@@ -10,8 +11,18 @@ const Home = () => {
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
+        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
       </div>
     </main>
   );
